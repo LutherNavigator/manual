05a29c2cb42cffcf7b749e2f50229e0ed3a5e36a
WKHAllen <wkhallen@gmail.com>
2021-04-15 19:18:52 -0500

    Open external links in new tab

---
+++ static/main.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", () => {
+  // Open all external links in new tab
+  const links = document.getElementsByTagName("a");
+  for (const link of links) {
+    if (link.host !== window.location.host) {
+      link.setAttribute("target", "_blank");
+    }
+  }
+});
